98780e51c722ef37dae0f306e331a309cba2fa93
André Gimenez Faria <andfaria13@gmail.com>
2017-06-20 17:24:02 -0300

    Added API config

---
+++ MiniGitClient/MiniGitClient/API/APIConfig.swift
@@ -0,0 +1,41 @@
+//
+//  APIConfig.swift
+//  MiniGitClient
+//
+//  Created by André Gimenez Faria on 20/06/17.
+//  Copyright © 2017 AGF. All rights reserved.
+//
+
+import Foundation
+
+enum Endpoint : String {
+    case search = "SEARCH"
+}
+
+protocol APIConfig {
+    func urlString(with endPoint : Endpoint) -> String?
+}
+
+struct MainAPIConfig : APIConfig {
+    
+    static let shared = MainAPIConfig()
+    
+    private var baseURL : String?
+    
+    private var endpointStrings : [String : String]?
+    
+    private init() {
+        if let endpointsFilePath = Bundle.main.path(forResource: "APIEndpoints", ofType: "plist"), let configDict = NSDictionary(contentsOfFile: endpointsFilePath) {
+            baseURL = configDict["BASE_URL"] as? String
+            endpointStrings = configDict["ENDPOINTS"] as? [String : String]
+        }
+    }
+    
+    func urlString(with endPoint : Endpoint) -> String? {
+        if let baseURL = baseURL, let endpoints = endpointStrings, let endpointURL = endpoints[endPoint.rawValue] {
+            return baseURL + endpointURL
+        }
+        return nil
+    }
+    
+}
---
+++ MiniGitClient/MiniGitClientTests/API/MainAPIConfigSpec.swift
@@ -0,0 +1,32 @@
+//
+//  MainAPIConfigSpec.swift
+//  MiniGitClient
+//
+//  Created by André Gimenez Faria on 20/06/17.
+//  Copyright © 2017 AGF. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import MiniGitClient
+
+class MainAPIConfigSpec: QuickSpec {
+    
+    override func spec() {
+        
+        describe("The Main API Config") { 
+            
+            context("knows", { 
+                
+                it("the search URL path") {
+                    guard let path = MainAPIConfig.shared.urlString(with: .search) else { fail(); return }
+                    expect(URL(string: path)).toNot(beNil())
+                }
+                
+            })
+            
+        }
+        
+    }
+    
+}
