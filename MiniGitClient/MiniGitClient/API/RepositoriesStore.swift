1e11e20dfcb1ab1f80b9dec8375625b18dd7eca9
André Gimenez Faria <andfaria13@gmail.com>
2017-06-22 23:34:16 -0300

    Added MainRepositoriesStore

---
+++ MiniGitClient/MiniGitClient/API/RepositoriesStore.swift
@@ -0,0 +1,42 @@
+//
+//  RepositoriesStore.swift
+//  MiniGitClient
+//
+//  Created by André Gimenez Faria on 20/06/17.
+//  Copyright © 2017 AGF. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import Alamofire
+import RxAlamofire
+
+protocol RepositoriesStore {
+    func swiftRepositories(forPage page : Int) -> Observable<[Repository]>
+}
+
+struct MainRepositoriesStore : RepositoriesStore {
+
+    static let shared = MainRepositoriesStore()
+    
+    private var config : APIConfig = MainAPIConfig.shared
+    
+    private init() { }
+    
+    func swiftRepositories(forPage page: Int) -> Observable<[Repository]> {
+        if let searchEndpoint = config.urlString(with: .search) {
+            let parameters : [String : Any] = ["q" : "language:Swift", "sort" : "stars", "page" : page]
+            return json(.get, searchEndpoint, parameters: parameters, encoding: URLEncoding.queryString)
+                   .map { json in
+                        if let jsonData = json as? [String : Any], let repositoriesList = jsonData["items"] as? [[String : Any]] {
+                            return repositoriesList.flatMap { Repository.init(json: $0) }
+                        }
+                        else {
+                            return [Repository]()
+                        }
+                   }
+        }
+        return Observable.just([])
+    }
+    
+}
---
+++ MiniGitClient/MiniGitClientTests/API/MainRepositoriesStoreSpec.swift
@@ -0,0 +1,53 @@
+//
+//  RepositoriesStore.swift
+//  MiniGitClient
+//
+//  Created by André Gimenez Faria on 22/06/17.
+//  Copyright © 2017 AGF. All rights reserved.
+//
+
+import Quick
+import Nimble
+import RxSwift
+
+@testable import MiniGitClient
+
+class MainRepositoriesStoreSpec: QuickSpec {
+    
+    private var disposeBag = DisposeBag()
+    
+    private var repositories : [Repository]?
+    
+    override func spec() {
+        
+        describe("The Repositories Store") { 
+            
+            context("calls the GitHub API", {
+                
+                beforeEach {
+                    self.repositories = nil
+                }
+                
+                it("and receives an Swift repositories list for a given page") {
+                    
+                    MainRepositoriesStore.shared.swiftRepositories(forPage: 1)
+                        .subscribe(
+                            onNext: { [weak self] in
+                                self?.repositories = $0
+                            },
+                            onError: { _ in
+                                fail()
+                            })
+                        .addDisposableTo(self.disposeBag)
+                    
+                    expect(self.repositories).toEventuallyNot(beNil(), timeout: 30.0, pollInterval: 10.0)
+                    
+                }
+                
+            })
+            
+        }
+        
+    }
+    
+}
