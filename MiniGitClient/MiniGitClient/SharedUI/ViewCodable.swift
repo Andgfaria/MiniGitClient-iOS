ab8f9f45de9167287c08788d7257ec7a5ef8dbaa
André Gimenez Faria <andfaria13@gmail.com>
2017-06-25 23:14:04 -0300

    Added ViewCodable protocol

---
+++ MiniGitClient/MiniGitClient/SharedUI/ViewCodable.swift
@@ -0,0 +1,45 @@
+//
+//  ViewCodable.swift
+//  MiniGitClient
+//
+//  Created by André Gimenez Faria on 25/06/17.
+//  Copyright © 2017 AGF. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ViewCodable {
+    func addViewsToHierarchy(_ views : [UIView])
+    func setupConstraints()
+    func setupStyles()
+    func bindComponents()
+}
+
+extension ViewCodable {
+    func setupContrainsts() { }
+    func setupStyles()      { }
+    func bindComponents()   { }
+}
+
+extension ViewCodable where Self : UIView {
+    
+    func addViewsToHierarchy(_ views : [UIView]) {
+        views.forEach {
+            self.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+}
+
+extension ViewCodable where Self : UIViewController {
+    
+    func addViewsToHierarchy(_ views : [UIView]) {
+        views.forEach {
+            self.view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+}
