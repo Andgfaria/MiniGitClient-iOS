2988f4a395993b9a3f2b3ea5b81d90f54e6ecb45
André Gimenez Faria <andre.faria@rga.com>
2017-07-04 10:45:46 -0300

    Tweaked RepositoryListInteractorSpec

--- MiniGitClient/MiniGitClientTests/Scenes/List/RepositoryListInteractorSpec.swift
+++ MiniGitClient/MiniGitClientTests/Scenes/List/RepositoryListInteractorSpec.swift
@@ -13,11 +13,18 @@ import RxSwift
 
 struct MockRepositoriesStore : RepositoriesStore {
     
+    var shouldFail = false
+    
     func swiftRepositories(forPage page: Int) -> Observable<(APIRequestResult,[Repository])> {
-        var testRepository = Repository()
-        testRepository.name = "Test"
-        testRepository.info = "Description"
-        return Observable.just((APIRequestResult.success,[testRepository]))
+        if shouldFail {
+            return Observable.just((APIRequestResult.networkError,[Repository]()))
+        }
+        else {
+            var testRepository = Repository()
+            testRepository.name = "Test"
+            testRepository.info = "Description"
+            return Observable.just((APIRequestResult.success,[testRepository]))
+        }
     }
     
 }
@@ -28,32 +35,38 @@ class RepositoryListInteractorSpec: QuickSpec {
         
         describe("The Repository List Interactor") {
             
-            var repositoryListInteractor : RepositoryListInteractor?
+            var repositoryListInteractor = RepositoryListInteractor()
+            
+            var mockRepositoryStore = MockRepositoriesStore()
             
             beforeEach {
-                repositoryListInteractor = nil
+                repositoryListInteractor = RepositoryListInteractor()
+                mockRepositoryStore = MockRepositoriesStore()
+                repositoryListInteractor.repositoriesStore = mockRepositoryStore
             }
-            
-            context("has", {
-                
-                it("a repositories variable") {
-                    repositoryListInteractor = RepositoryListInteractor()
-                    expect(repositoryListInteractor?.fetchResults.value.1).toNot(beNil())
-                }
-                
-            })
             
             context("can", { 
                 
                 it("fetch repositories incrementing a page property") {
-                    repositoryListInteractor = RepositoryListInteractor()
-                    repositoryListInteractor?.repositoriesStore = MockRepositoriesStore()
+                    repositoryListInteractor.loadRepositories()
+                    expect(repositoryListInteractor.fetchResults.value.0) == APIRequestResult.success
+                    expect(repositoryListInteractor.fetchResults.value.1.count) == 1
                     
-                    repositoryListInteractor?.loadRepositories()
-                    expect(repositoryListInteractor?.fetchResults.value.1.count) == 1
+                    repositoryListInteractor.loadRepositories()
+                    expect(repositoryListInteractor.fetchResults.value.0) == APIRequestResult.success
+                    expect(repositoryListInteractor.fetchResults.value.1.count) == 2
+                }
+                
+                it("keeps the repositories list safe after a request error") {
+                    repositoryListInteractor.loadRepositories()
                     
-                    repositoryListInteractor?.loadRepositories()
-                    expect(repositoryListInteractor?.fetchResults.value.1.count) == 2
+                    mockRepositoryStore.shouldFail = true
+                    repositoryListInteractor.repositoriesStore = mockRepositoryStore
+                
+                    repositoryListInteractor.loadRepositories()
+                    
+                    expect(repositoryListInteractor.fetchResults.value.0) == APIRequestResult.networkError
+                    expect(repositoryListInteractor.fetchResults.value.1.count) == 1
                 }
                 
             })
