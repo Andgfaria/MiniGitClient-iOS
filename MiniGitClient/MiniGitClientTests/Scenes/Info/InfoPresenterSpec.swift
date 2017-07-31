7aa53c0ad682486161dff4074f9d0bc6f42ba98f
André Faria <andre.faria@rga.com>
2017-07-31 13:35:37 -0300

    Finished InfoPresenterSpec

--- MiniGitClient/MiniGitClientTests/Scenes/Info/InfoPresenterSpec.swift
+++ MiniGitClient/MiniGitClientTests/Scenes/Info/InfoPresenterSpec.swift
@@ -48,28 +48,74 @@ class InfoPresenterSpec: QuickSpec {
                 presenter = InfoPresenter()
                 router = MockRouter()
                 tableView = UITableView(frame: .zero)
-                presenter.router = router
                 presenter.registerTableView(tableView)
+                presenter.router = router
             }
             
             context("as an UITableViewDataSource", { 
                 
                 it("returns 2 sections") {
-                    expect(tableView.numberOfSections) == 2
+                    expect(presenter.numberOfSections(in: tableView)) == 2
                 }
                 
                 it("returns 1 row for the first section") {
-                    expect(tableView.numberOfRows(inSection: 0)) == 1
+                    expect(presenter.tableView(tableView, numberOfRowsInSection: 0)) == 1
                 }
                 
                 it("returns 2 rows for the second section") {
-                    expect(tableView.numberOfRows(inSection: 1)) == 2
+                    expect(presenter.tableView(tableView, numberOfRowsInSection: 0)) == 1
                 }
                 
-                it("returns a default UITableViewCell for each row") {
+                it("returns basic cells for each row") {
+                    expect(presenter.tableView(tableView, cellForRowAt: IndexPath(row: 0, section: 0))).to(beAKindOf(UITableViewCell.self))
+                    expect(presenter.tableView(tableView, cellForRowAt: IndexPath(row: 0, section: 1))).to(beAKindOf(UITableViewCell.self))
+                    expect(presenter.tableView(tableView, cellForRowAt: IndexPath(row: 1, section: 1))).to(beAKindOf(UITableViewCell.self))
                 }
                 
             })
+            
+            context("as an UITableViewDelegate", { 
+                
+                it("returns a height of 56 for the first section") {
+                    expect(presenter.tableView(tableView, heightForRowAt: IndexPath(row: 0, section: 0))) == 56
+                }
+                
+                it("returns a height of 44 for the second section") {
+                    expect(presenter.tableView(tableView, heightForRowAt: IndexPath(row: 0, section: 1))) == 44
+                    expect(presenter.tableView(tableView, heightForRowAt: IndexPath(row: 1, section: 1))) == 44
+                }
+                
+                it("returns the me title for the first section header") {
+                    expect(presenter.tableView(tableView, titleForHeaderInSection: 0)) == R.string.info.meTitle()
+                }
+                
+                it("returns no title for the second section header") {
+                    expect(presenter.tableView(tableView, titleForHeaderInSection: 1)).to(beNil())
+                }
+                
+                it("returns the me descripton for the first section footer") {
+                    expect(presenter.tableView(tableView, titleForFooterInSection: 0)) == R.string.info.meDescription()
+                }
+                
+                it("returns no title for the second section footer") {
+                    expect(presenter.tableView(tableView, titleForFooterInSection: 1)).to(beNil())
+                }
+                
+            })
+            
+            context("router") {
+                
+                it("opens the GitHub page when the first row of the second section is selected") {
+                    presenter.tableView(tableView, didSelectRowAt: IndexPath(row: 0, section: 1))
+                    expect(router.didOpenGitHubPage).to(beTrue())
+                }
+                
+                it("opens the mail compose UI when the second row of the second section is selected") {
+                    presenter.tableView(tableView, didSelectRowAt: IndexPath(row: 1, section: 1))
+                    expect(router.didOpenMailCompose).to(beTrue())
+                }
+                
+            }
             
         }
         
