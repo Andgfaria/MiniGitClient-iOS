69905da9a6d2871ff98a7102a8b72245d3a041e1
André Gimenez Faria <andfaria13@gmail.com>
2017-08-02 22:59:40 -0300

    Added InfoTableViewModelSpec

--- MiniGitClient/MiniGitClientTests/Scenes/Info/InfoPresenterSpec.swift
+++ MiniGitClient/MiniGitClientTests/Scenes/Info/InfoPresenterSpec.swift
@@ -45,61 +45,9 @@ class InfoPresenterSpec: QuickSpec {
             beforeEach {
                 presenter = InfoPresenter()
                 router = MockRouter()
-                tableView = UITableView(frame: .zero)
                 presenter.router = router
             }
-//            
-//            context("as an UITableViewDataSource", { 
-//                
-//                it("returns 2 sections") {
-//                    expect(presenter.numberOfSections(in: tableView)) == 2
-//                }
-//                
-//                it("returns 1 row for the first section") {
-//                    expect(presenter.tableView(tableView, numberOfRowsInSection: 0)) == 1
-//                }
-//                
-//                it("returns 2 rows for the second section") {
-//                    expect(presenter.tableView(tableView, numberOfRowsInSection: 0)) == 1
-//                }
-//                
-//                it("returns basic cells for each row") {
-//                    expect(presenter.tableView(tableView, cellForRowAt: IndexPath(row: 0, section: 0))).to(beAKindOf(UITableViewCell.self))
-//                    expect(presenter.tableView(tableView, cellForRowAt: IndexPath(row: 0, section: 1))).to(beAKindOf(UITableViewCell.self))
-//                    expect(presenter.tableView(tableView, cellForRowAt: IndexPath(row: 1, section: 1))).to(beAKindOf(UITableViewCell.self))
-//                }
-//                
-//            })
-//            
-//            context("as an UITableViewDelegate", { 
-//                
-//                it("returns a height of 56 for the first section") {
-//                    expect(presenter.tableView(tableView, heightForRowAt: IndexPath(row: 0, section: 0))) == 56
-//                }
-//                
-//                it("returns a height of 44 for the second section") {
-//                    expect(presenter.tableView(tableView, heightForRowAt: IndexPath(row: 0, section: 1))) == 44
-//                    expect(presenter.tableView(tableView, heightForRowAt: IndexPath(row: 1, section: 1))) == 44
-//                }
-//                
-//                it("returns the me title for the first section header") {
-//                    expect(presenter.tableView(tableView, titleForHeaderInSection: 0)) == R.string.info.meTitle()
-//                }
-//                
-//                it("returns no title for the second section header") {
-//                    expect(presenter.tableView(tableView, titleForHeaderInSection: 1)).to(beNil())
-//                }
-//                
-//                it("returns the me descripton for the first section footer") {
-//                    expect(presenter.tableView(tableView, titleForFooterInSection: 0)) == R.string.info.meDescription()
-//                }
-//                
-//                it("returns no title for the second section footer") {
-//                    expect(presenter.tableView(tableView, titleForFooterInSection: 1)).to(beNil())
-//                }
-//                
-//            })
-//            
+
             context("router") {
                 
                 it("opens the GitHub page when the first row of the second section is selected") {
